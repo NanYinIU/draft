27ff7ab036a6cc8ee583cea915fc047b1b4bad90
gaoguoxing <nanyiniu76@gmail.com>
2024-08-02 10:36:34 +0800

    fix calc_new_position

--- src/editor.rs
+++ src/editor.rs
@@ -46,47 +46,43 @@ fn draw_row(out: &mut io::Stdout, message: String) -> Result<(), io::Error> {
     move_cursor(out, Move2::default())
 }
 
-fn draw_row_2(out: &mut io::Stdout) -> Result<(), io::Error> {
-    let (column, row) = cursor::position()?;
-    execute!(out, cursor::MoveTo(column + 1, row), EnableBlinking)?;
-    Ok(())
-}
-
 fn move_cursor(out: &mut io::Stdout, move2: Move2) -> Result<(), io::Error> {
     let (column, row) = calc_new_position(cursor::position()?, move2);
-    println!("after calc column and row is {:?}", (column, row));
+    // println!("after calc column and row is {:?}", (column, row));
     execute!(out, cursor::MoveTo(column, row), EnableBlinking)?;
     Ok(())
 }
 pub fn calc_new_position(position: (u16, u16), move2: Move2) -> (u16, u16) {
     let mut column = position.0;
     let mut row = position.1;
+    // println!("before calc column and row is {:?}", (column, row));
     return match move2.direction {
-        Direction::UP => {
-            row = row - move2.offset;
+        Direction::Up => {
+            row -= move2.offset;
             (column, row)
         }
-        Direction::DOWN => {
-            row = row + move2.offset;
+        Direction::Down => {
+            row += move2.offset;
             (column, row)
         }
-        Direction::LEFT => {
-            column = column + move2.offset;
+        Direction::Left => {
+            column -= move2.offset;
             (column, row)
         }
-        Direction::RIGHT => {
-            column = column - move2.offset;
+        Direction::Right => {
+            column += move2.offset;
             (column, row)
         }
     };
 }
 
 enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
+    Up,
+    Down,
+    Left,
+    Right,
 }
+
 type Offset = u16;
 
 pub struct Move2 {
@@ -96,17 +92,17 @@ pub struct Move2 {
 
 impl Move2 {
     // 移动右1
-    pub fn new(direction: Direction, offset: Offset) -> Self {
-        return Move2 { direction, offset };
+    fn new(direction: Direction, offset: Offset) -> Self {
+        Move2 { direction, offset }
     }
 
     pub fn backspace() -> Self {
-        Self::new(Direction::LEFT, 1)
+        Self::new(Direction::Left, 1)
     }
 }
 
 impl Default for Move2 {
     fn default() -> Self {
-        Self::new(Direction::RIGHT, 1)
+        Self::new(Direction::Right, 1)
     }
 }
