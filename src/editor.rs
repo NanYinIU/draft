af9f5f5a2a97940b30e092425e1ed7e1553a55bc
gaoguoxing <nanyiniu76@gmail.com>
2024-08-07 21:11:26 +0800

    add editor/view/buffer

--- src/editor.rs
+++ src/editor.rs
@@ -1,17 +1,28 @@
 use std::io;
+use std::io::Write;
+use std::sync::Arc;
 
+use crossterm::cursor::MoveTo;
 use crossterm::event::read;
 use crossterm::event::Event;
 use crossterm::event::KeyCode;
 use crossterm::event::KeyEvent;
 use crossterm::event::KeyModifiers;
+use crossterm::style::Print;
+use crossterm::QueueableCommand;
 
+use crate::terminal;
+use crate::terminal::Size;
 use crate::terminal::Terminal;
 
 pub fn run() -> Result<(), io::Error> {
-    let mut terminal = Terminal::default().unwrap();
-    terminal.welcome()?;
-    // terminal.clear_screen()?;
+    let mut editor = Editor::default();
+    let mut terminal = editor.terminal;
+    terminal.clear_screen()?;
+    editor.is_clear = true;
+    editor.view.render(&mut terminal)?;
+    // terminal.welcome()?;
+
     // terminal.draw_row()?;
     while let Event::Key(event) = read()? {
         if KeyEvent::new(KeyCode::Char('q'), KeyModifiers::CONTROL) == event {
@@ -28,19 +39,24 @@ pub fn run() -> Result<(), io::Error> {
             terminal.clear_line()?.flush()?;
             continue;
         }
+        if editor.is_clear {
+            terminal.clear_screen()?;
+            editor.is_clear = false;
+        }
         match event.code {
             KeyCode::Enter => terminal.draw_row()?,
             KeyCode::Backspace => {
-                let (column, row) = terminal.move_curor_to(Direction::Left, 1)?;
+                let (column, row) = terminal.move_direction_to(Direction::Left, 1)?;
+                // println!("this (column, row) is {:?}", (column, row));
                 terminal.clear_line_purge()?;
-                if column == 0 && row > 1 {
+                if column == 0 && row >= 1 {
                     // 向上一行
-                    terminal.move_curor_to(Direction::Right, 1)?;
+                    terminal.move_direction_to(Direction::Up, 1)?;
                     // 移动到上一行的末尾位置？
                 }
             }
             KeyCode::Tab => {
-                terminal.move_curor_to(Direction::Right, 4)?;
+                terminal.move_direction_to(Direction::Right, 4)?;
             }
             KeyCode::Char(c) => terminal.print(c)?,
             _ => {}
@@ -93,5 +109,70 @@ impl MoveAction {
 
     pub fn get_target_position(&mut self) -> (u16, u16) {
         self.target_position
+    }
+}
+
+pub struct Editor {
+    terminal: Terminal,
+    view: View,
+    is_clear: bool,
+}
+impl Default for Editor {
+    fn default() -> Self {
+        let terminal = Terminal::default().unwrap();
+        let is_clear = true;
+        Editor {
+            terminal,
+            view: View::default(),
+            is_clear,
+        }
+    }
+}
+pub struct View {
+    buffer: Buffer,
+}
+
+impl Default for View {
+    fn default() -> Self {
+        // 渲染View，前面加 ~ 这种
+        View {
+            buffer: Buffer::default(),
+        }
+    }
+}
+impl View {
+    pub fn render(&self, terminal: &mut Terminal) -> Result<(), io::Error> {
+        let terminal_size: Size = terminal.size.clone();
+        let h = terminal_size.height;
+        let w = terminal_size.width;
+        let start_h = terminal_size.height / 2 as u16;
+        for line in 0..h {
+            terminal.move_to((0, line))?;
+            terminal.print("~\r")?;
+            if let Some(b_line) = self.buffer.lines.get(line as usize) {
+                let line_len = b_line.len();
+                let column_start = (w - u16::try_from(line_len).unwrap()) / 2;
+                terminal.move_to((column_start, start_h + line - 1))?;
+                terminal.print(b_line)?;
+            }
+        }
+
+        terminal.flush()?;
+
+        Ok(())
+    }
+}
+pub struct Buffer {
+    lines: Vec<String>,
+}
+
+impl Default for Buffer {
+    fn default() -> Self {
+        let welcome_title = String::from("welcome use draft!");
+        let version_content = String::from("version 0.0.1 ");
+        let author = String::from("by author<nanyin>");
+        let buf = vec![welcome_title, version_content, author];
+
+        Buffer { lines: buf }
     }
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -2,7 +2,7 @@ use crate::editor::{self, MoveAction};
 use crossterm::{
     cursor::{self, EnableBlinking, MoveTo, MoveToNextLine, SavePosition},
     style::Print,
-    terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType},
+    terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType, ScrollUp},
     QueueableCommand,
 };
 use std::{
@@ -10,13 +10,14 @@ use std::{
     io::{self, stdout, Write},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
+
 pub struct Terminal {
-    size: Size,
+    pub size: Size,
     pub _stdout: io::Stdout,
 }
 
@@ -39,15 +40,17 @@ impl Terminal {
     }
 
     pub fn draw_row(&mut self) -> Result<(), io::Error> {
-        self._stdout
-            .queue(MoveToNextLine(1))?
-            .queue(SavePosition)?
-            .queue(Print("~\r"))?
-            .flush()?;
+        let (_, row) = cursor::position()?;
+        if row > self.size.height / 2 {
+            self._stdout.queue(ScrollUp(1))?.queue(MoveTo(0, row))?;
+        } else {
+            self._stdout.queue(MoveToNextLine(1))?;
+        }
+        self._stdout.queue(SavePosition)?.flush()?;
         Ok(())
     }
 
-    pub fn move_curor_to(
+    pub fn move_direction_to(
         &mut self,
         direction: editor::Direction,
         step: u16,
@@ -55,35 +58,19 @@ impl Terminal {
         let position = cursor::position()?;
         let mut move_action = MoveAction::new(direction, position, step);
         let (c, r) = move_action.to_move().get_target_position();
-        self._stdout.queue(MoveTo(c, r))?.flush()?;
+        self.move_to((c, r))?;
         Ok((c, r))
     }
 
-    pub fn welcome(&mut self) -> Result<(), io::Error> {
-        // 打印欢迎标语，在当前Terminal的正中间
-        let width = self.size.width;
-        let height = self.size.height;
-        let welcome_title = "welcome use draft!";
-        let welcome_title_len = welcome_title.len();
-        let column_start = (width - u16::try_from(welcome_title_len).unwrap()) / 2;
-        println!("Terminal size is:{:?}", self.size);
-        let command = self._stdout.queue(Clear(ClearType::All))?;
-        let mut i = 0;
-        while i < height {
-            command.queue(Print("~\n\r"))?;
-            i += 1;
-        }
-        command
-            .queue(MoveTo(column_start, height / 2))?
-            .queue(Print(welcome_title))?
-            .queue(MoveTo(0, 0))?;
-        self.flush()?;
+    pub fn move_to(&mut self, (column, row): (u16, u16)) -> Result<(), io::Error> {
+        self._stdout.queue(MoveTo(column, row))?.flush()?;
         Ok(())
     }
+
     pub fn clear_screen(&mut self) -> Result<(), io::Error> {
         self._stdout
             .queue(Clear(ClearType::All))?
-            .queue(MoveTo(0, 1))?
+            .queue(MoveTo(0, 0))?
             .queue(Print("~\r"))?
             .queue(EnableBlinking)?;
         // self.draw_row()?;
